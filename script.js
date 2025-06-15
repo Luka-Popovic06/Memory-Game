784b34ef8ca005f44383508d0d30c6db0f32abfe
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-15 21:37:53 +0200

    Adding variables in JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+let cubeArray = [];
+const cubes = document.querySelector('.cubes');
+const btnEasy = document.querySelector('.easy-mode');
+const btnMedium = document.querySelector('.medium-mode');
+const btnHard = document.querySelector('.hard-mode');
+let cubeNumber;
+//
+addingNewObject(3);
+createGrid(3);
+//
