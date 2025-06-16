8dd7a05e4ff70369c54a8c7ca82d70484b3a919f
Luka Popovic <lukapopovic0111@gmail.com>
2025-06-16 23:39:55 +0200

    Function shuffle

--- script.js
+++ script.js
@@ -82,3 +82,10 @@ cubes.addEventListener('click', function (e) {
 
   createGrid();
 });
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    [array[i], array[random]] = [array[random], array[i]];
+  }
+}
