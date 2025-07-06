7bdd0fa2081f7cada26ce8ad6d3a217462585774
Luka Popovic <lukapopovic0111@gmail.com>
2025-07-06 20:12:37 +0200

    Update btnControle

--- script.js
+++ script.js
@@ -1,43 +1,31 @@
 'use strict';
 let cubeArray = [];
 const cubes = document.querySelector('.cubes');
-const btnEasy = document.querySelector('.easy-mode');
-const btnMedium = document.querySelector('.medium-mode');
-const btnHard = document.querySelector('.hard-mode');
 const startingScore = document.querySelector('.starting-clicks');
 const maxClick = document.querySelector('.max-clicks');
+const btnControle = document.querySelectorAll('.btn-control');
 let cubeNumber;
+let cubeColor;
 let score = 0;
 //
 addingNewObject(3);
 createGrid(3);
-//
-btnEasy.addEventListener('click', function (e) {
-  cubeNumber = e.target.value;
-  maxClick.textContent = cubeNumber * cubeNumber;
-  addingNewObject(cubeNumber);
-  createGrid(cubeNumber);
+
+btnControle.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    cubeNumber = e.target.value;
+    maxClick.textContent = cubeNumber * cubeNumber;
+    addingNewObject(cubeNumber);
+    createGrid(cubeNumber);
+    score = 0;
+    startingScore.textContent = score;
+  });
 });
-btnMedium.addEventListener('click', function (e) {
-  cubeNumber = e.target.value;
-  maxClick.textContent = cubeNumber * cubeNumber;
-  addingNewObject(cubeNumber);
-  createGrid(cubeNumber);
-});
-btnHard.addEventListener('click', function (e) {
-  cubeNumber = e.target.value;
-  maxClick.textContent = cubeNumber * cubeNumber;
-  addingNewObject(cubeNumber);
-  createGrid(cubeNumber);
-});
+
 function addingNewObject(number) {
   cubeArray = [];
   for (let i = 0; i < number * number; i++) {
-    let red = Math.trunc(Math.random() * 255) + 1;
-    let blue = Math.trunc(Math.random() * 255) + 1;
-    let yellow = Math.trunc(Math.random() * 255) + 1;
-    let cubeColor = `RGB(${red}, ${blue}, ${yellow})`;
-
+    coloringCubes();
     const NewObject = {
       class: 'cube',
       id: crypto.randomUUID(),
@@ -47,10 +35,11 @@ function addingNewObject(number) {
     cubeArray.push(NewObject);
   }
 }
+
 function createGrid(size) {
   cubes.innerHTML = '';
   cubeArray.forEach(cube => {
-    cubes.innerHTML += `<div class="${cube.class}" id="${cube.id}"></div>`;
+    cubes.innerHTML += `<div style="bacgroundColor=${cube.color}" class="${cube.class}" id="${cube.id}"></div>`;
     const cubeId = document.getElementById(cube.id);
     cubeId.style.backgroundColor = cube.color;
   });
@@ -61,25 +50,24 @@ function createGrid(size) {
 cubes.addEventListener('click', function (e) {
   const cube = e.target.closest('.cube');
   if (e.target.closest('.cube')) {
-    const newCubeArray = cubeArray.find(c => c.id === cube.id);
-    if (newCubeArray.isClicked === false) {
-      newCubeArray.isClicked = true;
+    const oneCube = cubeArray.find(c => c.id === cube.id);
+    if (oneCube.isClicked === false) {
+      oneCube.isClicked = true;
       score++;
       startingScore.textContent = score;
-      if (score === Number(maxClick.textContent)) {
+      if (score === cubeArray.length) {
         alert('You win!!!!');
         score = 0;
         startingScore.textContent = score;
       }
-    } else if (newCubeArray.isClicked === true) {
+    } else if (oneCube.isClicked === true) {
       alert('You lose! Try again!');
       score = 0;
+      oneCube.isClicked = false;
       startingScore.textContent = score;
     }
+    shuffle(cubeArray);
   }
-
-  shuffle(cubeArray);
-
   createGrid();
 });
 
